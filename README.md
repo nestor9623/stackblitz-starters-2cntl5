c14f0483e542983847ce0c779b221f027229b1e7
EX366505 <nestorandres.franco@empresas.justicia.es>
2024-03-18 12:50:48 +0100

    Fondo , Readme , Sanetizar
    agregar imagenes sanetizadas  , archivo readme actualizado y pequeña configuración de estilos

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # stackblitz-starters-2cntl5
 
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nestor9623/stackblitz-starters-2cntl5)
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nestor9623/stackblitz-starters-2cntl5)ç
+
+# Ejecución o arranque de la aplicacion
+
+Basta con ejecutar los comandos : ng serve or npm run
+
+# Para los Test que estan hechos con Jest
+
+Se debe ejecutar el comando npm run test
+
+# Gif
+
+Para el gif se monto la configuración en base 64 sanetizando el codigo , se hizo tambien desde los estilos o podría hacerlo con la url sanetizada...pero el navegador te da un error de cors.
+
+# Estilos
+
+Se creo el fichero Styles.scss aplicando algo minimo de reglas de desarrollo como BEM , OOCSS y SMACSS
+
+# Jest
+
+Pruebas unitarias con jest , se implemento una configuracíon basica. Por tiempos no se alcanzo a montar pruebas e2e. Los test se pueden ver en login.component.spect.ts
+
+# Adiciones
+
+Creo un interceptor para mostrar un spiner en un component , esto quizas con la idea de hacer el login con una api free de usuarios. Asi mismo se empleo un router validando la ruta con un guard.
--- src/components/post-home/post-home.component.ts
+++ src/components/post-home/post-home.component.ts
@@ -1,16 +1,19 @@
 import { AuthService } from './../../services/auth.service';
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { image } from '../../shared/images/fondo.const';
 
 @Component({
-    selector: 'app-post-home',
-    standalone: true,
-    imports: [CommonModule, ReactiveFormsModule],
-    template: `
-    <div class="fondo_principal">          
-        <div class="fondo" id="fondo">
+  selector: 'app-post-home',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  template: `
+    <div class="fondo_principal">
+        <div class="fondo" id="fondo" [ngStyle]="stylesObj">
+          <img [src]="imageSource" alt="Image Source"  style="width: 100%;  height: auto; max-height: 99%;"/>
             <div class="messages">
                 <label class="messages--big">{{titlePage}}</label>
                 <div class="form-group">
@@ -18,16 +21,39 @@ import { Router } from '@angular/router';
                 </div>
             </div>
         </div>
-  `
+  `,
 })
-
 export class PostHomeComponent {
-    titlePage = 'Logueado correctamente';
+  imageSource: any;
+  titlePage = 'Logueado correctamente';
+  //TODO:setear el fondo mediante la url en este objeto de propiedades
+  stylesObj = {
+    'background-image': '',
+    'background-size': 'cover',
+    position: 'relative',
+    height: '100%',
+    width: '100%',
+  };
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private sanitizer: DomSanitizer
+  ) {
+    this.getImagenByUrl();
+  }
 
-    constructor(private authService: AuthService, private router: Router) { }
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('');
+  }
 
-    logout() {
-        this.authService.logout();
-        this.router.navigateByUrl('');
-    }
+  getImagenByUrl() {
+    this.imageSource = this.sanitizer.bypassSecurityTrustResourceUrl(
+      `data:image/png;base64, ${image}`
+    );
+    this.stylesObj = {
+      ...this.stylesObj,
+      'background-image': 'url(' + this.imageSource + ')',
+    };
+  }
 }
